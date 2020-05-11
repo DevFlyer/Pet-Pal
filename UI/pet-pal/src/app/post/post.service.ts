9fa855c01dc9716d04c733b44daaf34f60ef526d
Jamie Introcaso <jamie.introcaso@gmail.com>
2020-05-11 19:37:02 -0400

    update api address in UI project

--- UI/pet-pal/src/app/post/post.service.ts
+++ UI/pet-pal/src/app/post/post.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
   private apiBase: string;
   private postEndpoint: string;
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    this.apiBase = environment.baseApi;
+   }
   
   savePost(post: Post):Observable<Post> {
+    this.postEndpoint = 'post';
     return this.httpClient.post<Post>(`${this.apiBase}${this.postEndpoint}`, post);
   }
 }
